b512d1d048a3c153b1a8e3097dab4306de604371
Ryan Cantin <ryan.cantin@coxautoinc.com>
2022-11-23 13:33:48 -0500

    Added Anthony and Sarah Zoom links

--- src/components/OnlineLinks.js
+++ src/components/OnlineLinks.js
@@ -5,7 +5,11 @@ class OnlineLinks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      onlineclasses: [{ ord: 1, instructor: "Kayleigh", meetingid: "851 1656 6456", passcode: "ballet", zoomlink: "https://us02web.zoom.us/j/85116566456?pwd=QTNVL05LbWlzMEE2UytmYlFkZWptUT09" }],
+      onlineclasses: [
+        { ord: 1, instructor: "Kayleigh", meetingid: "851 1656 6456", passcode: "ballet", zoomlink: "https://us02web.zoom.us/j/85116566456?pwd=QTNVL05LbWlzMEE2UytmYlFkZWptUT09" },
+        { ord: 2, instructor: "Anthony", meetingid: "835 0321 5828", passcode: "499543", zoomlink: "https://us02web.zoom.us/j/83503215828?pwd=ZkdUdElTSVd2MnlML3c2S1dweDlEZz09" },
+        { ord: 3, instructor: "Sarah", meetingid: "854 6627 8655", passcode: "dreamdance", zoomlink: "https://us02web.zoom.us/j/85466278655?pwd=TmJUNzl1T1VFd3ByTHZhclRrNVJIUT09" },
+      ],
     };
   }
 
@@ -23,7 +27,7 @@ class OnlineLinks extends React.Component {
           </div>
           <div className="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-4 g-4 g-xl-5">
             {this.state.onlineclasses
-              .sort((a, b) => (a.ord > b.ord ? 1 : -1))
+              .sort((a, b) => (a.instructor > b.instructor ? 1 : -1))
               .map((item, c) => (
                 <div key={c} className="col">
                   <div className="card shadow">
